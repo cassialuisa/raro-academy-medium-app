81a288244e7ca36b035cdbcfa777c2fa987e0b96
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-03-31 18:15:31 -0300

    Implementa o card para a tela sem artigos.

--- src/pages/Artigos/index.tsx
+++ src/pages/Artigos/index.tsx
@@ -2,22 +2,24 @@ import { useEffect, useState } from "react";
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
 import { geraArtigos } from "../../stories/helpers/gerador-artigos";
+import { SemArtigos } from "../../components/SemArtigos";
+
 
 export const ArtigosPage = () => {
   const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
 
   useEffect(() => {
-    setArticles(geraArtigos(5));
+    setArticles(geraArtigos(10));
   }, []);
 
   if (geraArtigos(0)) {
     <div >
-      <h1>Ainda não temos artigos publicados</h1>
+      <SemArtigos/>
     </div>
   }
   return (
     <div className="my-30">
-      <ArticleList
+      //<ArticleList
         articles={articles}
       />
     </div>
