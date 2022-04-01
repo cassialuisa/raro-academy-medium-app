17f39e6d0caf095ccf7de02cad76cfa6657d3936
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-04-01 18:38:22 -0300

    implementa processo de autenticação do usuário

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,28 @@
 import { Button } from "../Button";
 import { Input } from "../Input";
+import { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  async function autenticaUsuario(event: React.FormEvent<HTMLFormElement>) {
+
+    //essa função impede que o form execute seu comportamento padrão de submit > reload. 
+    //deve ser aplicada ao form por completo e não ao botão de submit. 
+    event.preventDefault();
+    const url = `http://3.221.159.196:3307/auth/login`;
+    const response = await axios.post(url,{ login, senha });
+
+    const { access_token, id } = response.data;
+      if (access_token) {
+    navigate("/artigos");
+      }
+  }
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -12,7 +33,7 @@ export const Login = () => {
             alt="Workflow"
           />
         </div>
-        <form className="mt-8 space-y-6" action="#">
+        <form className="mt-8 space-y-6" onSubmit={autenticaUsuario}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div className="mt-5">
               <Input
@@ -21,6 +42,8 @@ export const Login = () => {
                 label="Login"
                 placeholder="login"
                 required
+                value={login}
+                onChange={(event) => setLogin(event.target.value)}
               />
             </div>
 
@@ -31,10 +54,13 @@ export const Login = () => {
                 label="senha"
                 placeholder="********"
                 required
+                value={senha}
+                onChange={(event) => setSenha(event.target.value)}
               />
             </div>
           </div>
           <div>
+
             <Button type="submit">Login</Button>
           </div>
         </form>
