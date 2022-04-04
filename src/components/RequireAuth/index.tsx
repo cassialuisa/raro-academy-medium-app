fcc10eaecdb8131ed879084527f15b1b3946cc60
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-04-04 18:27:36 -0300

    Impede user anônimo de acessar rota /artigos.

---
+++ src/components/RequireAuth/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+export const RequireAuth: React.FC = () => {
+    
+    const [isAuthenticated, setAuthenticated] = useState(true);
+    useEffect(() => {
+      setAuthenticated(localStorage.getItem("access_token") !== null);
+    }, []);
+  
+    if (!isAuthenticated) {
+      return <Navigate to="/" />
+    }
+  
+    return <Outlet />;
+  }
